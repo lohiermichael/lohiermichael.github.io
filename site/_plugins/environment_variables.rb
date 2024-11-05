fa226bae864c54aaf7f356bbffed9e85389ca112
Michael Lohier <lohiermichael@gmail.com>
2024-11-05 18:17:06 +0100

    plugins: format the environment variables file
    Signed-off-by: Michael Lohier <lohiermichael@gmail.com>

--- site/_plugins/environment_variables.rb
+++ site/_plugins/environment_variables.rb
@@ -3,17 +3,26 @@ require 'dotenv'
 module Jekyll
   class EnvironmentVariablesGenerator < Generator
     def generate(site)
-      # Load environment variables from root .env file
+      # Load environment variables from root .env file 
       if Jekyll.env == "development"
         root_dir = File.expand_path('../../', File.dirname(__FILE__))
         env_file = File.join(root_dir, '.env')
-
+        
         if File.exist?(env_file)
           Dotenv.load(env_file)
-          Jekyll.logger.info "Environment:", "Loaded .env file successfully"
-          Jekyll.logger.info "Environment:", "FORMSPREE_ID is set to: #{ENV['FORMSPREE_ID']}"
+          Jekyll.logger.info(
+            "Environment:", 
+            "Loaded .env file successfully"
+          )
+          Jekyll.logger.info(
+            "Environment:",
+            "FORMSPREE_ID is set to: #{ENV['FORMSPREE_ID']}"
+          )
         else
-          Jekyll.logger.warn "Environment:", ".env file not found at: #{env_file}"
+          Jekyll.logger.warn(
+            "Environment:",
+            ".env file not found at: #{env_file}"
+          )
         end
       end
 
@@ -24,7 +33,10 @@ module Jekyll
       end
       
       # Debug output
-      Jekyll.logger.info "Environment:", "Variables available in site.env: #{site.config['env'].keys.join(', ')}"
+      Jekyll.logger.info(
+        "Environment:",
+        "Variables available in site.env: #{site.config['env'].keys.join(', ')}"
+      )
     end
   end
 end
